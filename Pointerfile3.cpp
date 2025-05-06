234e2e25ac72cae850eff74df48bbe120b34031b
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2025-05-06 15:28:50 +0700

    Mengisi int main dengan cara untuk manggil object mahasiswa

--- Pointerfile3.cpp
+++ Pointerfile3.cpp
@@ -11,9 +11,11 @@ class mahasiswa
         }
 };
 
-
-
 int main()
 {
-
+    mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
 }
