39beff07ee75f6be7bd59e2e625c0caf65fa0ee9
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2025-05-06 15:03:08 +0700

    Mengisi int main

--- Pointerfile1.cpp
+++ Pointerfile1.cpp
@@ -41,6 +41,15 @@ class Lingkaran
 };
 
 
-int main(){
+int main()
+{
+    PersegiPanjang objekPP;
+    Lingkaran ol;
 
+    cout << " Masukkan Panjang : ";
+    cin >> objekPP.panjang;
+    cout << " Masukkan Lebar : ";
+    cin >> objekPP.lebar;
+    cout << " Luas Persegi Panjang : " << 
+    objekPP.luasPp(objekPP.panjang,objekPP.lebar) << endl;
 }
