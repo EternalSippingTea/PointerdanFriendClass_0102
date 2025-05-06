c9262fbff720de61b76a123c353fc5564da3dff9
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2025-05-06 15:21:24 +0700

    Mengisi int main dengan beda cara untuk manggil object mahasiswa ( biasa, reference, dereference)

--- Pointerfile2.cpp
+++ Pointerfile2.cpp
@@ -12,5 +12,15 @@ class mahasiswa
 
 int main()
 {
+    mahasiswa mhs{1};  // Object mhs
+    mhs.showNim();  // Member Access Operator
 
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
+    pMhs->nim = 3; // Member Access Operator
+    pMhs->showNim();
+    return 0;
 }
